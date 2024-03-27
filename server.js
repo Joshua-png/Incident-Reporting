ac57a47373567187bd6816d978bfcbe27461c4da
Joshua Aryee <profjoshua65@gmail.com>
2024-03-27 02:37:26 +0000

    assigned a homepage

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 // Serve Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Middleware
 app.use(express.json());
