464204d157b13b5c725bfe89453367db456ec987
Joshua Aryee <profjoshua65@gmail.com>
2024-03-28 07:48:11 +0000

    edits to test

--- test/incident.test.js
+++ test/incident.test.js
@@ -14,7 +14,244 @@ before(async () => {
   await incident.destroy({ where: {} });
 });
 
+const incidents = [
+  {
+    incident_desc: "Typoon destroyed all the expensive houses",
+    city: "Washington",
+    country: "USA",
+    date: "2024-03-28T06:33:14.974Z",
+    weather_report: {
+      coord: {
+        lon: -120.5015,
+        lat: 47.5001,
+      },
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "overcast clouds",
+          icon: "04n",
+        },
+      ],
+      base: "stations",
+      main: {
+        temp: 275.48,
+        feels_like: 273.9,
+        temp_min: 274.29,
+        temp_max: 277.95,
+        pressure: 1009,
+        humidity: 86,
+        sea_level: 1009,
+        grnd_level: 922,
+      },
+      visibility: 10000,
+      wind: {
+        speed: 1.61,
+        deg: 258,
+        gust: 1.39,
+      },
+      clouds: {
+        all: 100,
+      },
+      dt: 1711607594,
+      sys: {
+        type: 2,
+        id: 2003593,
+        country: "US",
+        sunrise: 1711547367,
+        sunset: 1711592680,
+      },
+      timezone: -25200,
+      id: 5815135,
+      name: "Washington",
+      cod: 200,
+    },
+  },
+
+  {
+    incident_desc: "Typoon destroyed all the expensive houses",
+    city: "London",
+    country: "UK",
+    date: "2024-03-28T06:33:14.974Z",
+    weather_report: {
+      coord: {
+        lon: -120.5015,
+        lat: 47.5001,
+      },
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "overcast clouds",
+          icon: "04n",
+        },
+      ],
+      base: "stations",
+      main: {
+        temp: 275.48,
+        feels_like: 273.9,
+        temp_min: 274.29,
+        temp_max: 277.95,
+        pressure: 1009,
+        humidity: 86,
+        sea_level: 1009,
+        grnd_level: 922,
+      },
+      visibility: 10000,
+      wind: {
+        speed: 1.61,
+        deg: 258,
+        gust: 1.39,
+      },
+      clouds: {
+        all: 100,
+      },
+      dt: 1711607594,
+      sys: {
+        type: 2,
+        id: 2003593,
+        country: "UK",
+        sunrise: 1711547367,
+        sunset: 1711592680,
+      },
+      timezone: -25200,
+      id: 5815135,
+      name: "London",
+      cod: 200,
+    },
+  },
+
+  {
+    incident_desc: "Typoon destroyed all the expensive houses",
+    city: "Accra",
+    country: "Ghana",
+    date: "2024-03-28T06:33:14.974Z",
+    weather_report: {
+      coord: {
+        lon: -120.5015,
+        lat: 47.5001,
+      },
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "overcast clouds",
+          icon: "04n",
+        },
+      ],
+      base: "stations",
+      main: {
+        temp: 275.48,
+        feels_like: 273.9,
+        temp_min: 274.29,
+        temp_max: 277.95,
+        pressure: 1009,
+        humidity: 86,
+        sea_level: 1009,
+        grnd_level: 922,
+      },
+      visibility: 10000,
+      wind: {
+        speed: 1.61,
+        deg: 258,
+        gust: 1.39,
+      },
+      clouds: {
+        all: 100,
+      },
+      dt: 1711607594,
+      sys: {
+        type: 2,
+        id: 2003593,
+        country: "Ghana",
+        sunrise: 1711547367,
+        sunset: 1711592680,
+      },
+      timezone: -25200,
+      id: 5815135,
+      name: "Accra",
+      cod: 200,
+    },
+  },
+
+  {
+    incident_desc: "Typoon destroyed all the expensive houses",
+    city: "Washington",
+    country: "USA",
+    date: "2024-03-28T06:33:14.974Z",
+    weather_report: {
+      coord: {
+        lon: -120.5015,
+        lat: 47.5001,
+      },
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "overcast clouds",
+          icon: "04n",
+        },
+      ],
+      base: "stations",
+      main: {
+        temp: 275.48,
+        feels_like: 273.9,
+        temp_min: 274.29,
+        temp_max: 277.95,
+        pressure: 1009,
+        humidity: 86,
+        sea_level: 1009,
+        grnd_level: 922,
+      },
+      visibility: 10000,
+      wind: {
+        speed: 1.61,
+        deg: 258,
+        gust: 1.39,
+      },
+      clouds: {
+        all: 100,
+      },
+      dt: 1711607594,
+      sys: {
+        type: 2,
+        id: 2003593,
+        country: "US",
+        sunrise: 1711547367,
+        sunset: 1711592680,
+      },
+      timezone: -25200,
+      id: 5815135,
+      name: "Washington",
+      cod: 200,
+    },
+  },
+];
+
 describe("Incident Routes", () => {
+  it("Get All incidents and filter (No query specified)", async () => {
+    await incident.bulkCreate(incidents);
+
+    const res = await chai.request(server).get("/api/v1/incidents");
+
+    expect(res).to.have.status(200);
+    res.body.incidents.should.be.a("array");
+    expect(res.body.incidents.length).to.equal(4);
+  });
+
+  it("Get All incidents and filter (city not in the database specified)", async () => {
+    await incident.bulkCreate(incidents);
+
+    const res = await chai
+      .request(server)
+      .get("/api/v1/incidents/?city=washingston"); //washington with an s
+    // .query({ city: "London" });
+
+    expect(res).to.have.status(200);
+    res.body.incidents.should.be.a("array");
+    expect(res.body.incidents.length).to.equal(0);
+  });
+
   it("Create a new Incident", (done) => {
     chai
       .request(server)
